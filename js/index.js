c1188afcafa465e747e43e4caf12b65c6c42c809
Valeria Meza <valeriameza173@gmail.com>
2020-04-22 18:51:02 -0500

    used ajax to request information from the api

--- js/index.js
+++ js/index.js
@@ -1,2 +1,17 @@
+"use strict";
 
+(function() {
+   $(document).ready(function(){
+       console.log('DOM is ready');
 
+       $.ajax('https://api.chucknorris.io/jokes/random').done(function(data, status){
+           console.log('Everything is looking good.', status)
+           $('.joke').text(data.value);
+       }).fail(function(status, error) {
+           console.log('oops something went wrong.', status);
+           console.log(error);
+       }).always(function() {
+           console.log('this always runs');
+       })
+   });
+})();
