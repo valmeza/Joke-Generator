88b581c45da7071c2790d5357a8a721d2def6777
Valeria Meza <valeriameza173@gmail.com>
2020-04-22 19:03:27 -0500

    completed a simple joke generator

--- js/index.js
+++ js/index.js
@@ -1,17 +1,21 @@
 "use strict";
 
-(function() {
-   $(document).ready(function(){
-       console.log('DOM is ready');
-
-       $.ajax('https://api.chucknorris.io/jokes/random').done(function(data, status){
-           console.log('Everything is looking good.', status)
-           $('.joke').text(data.value);
-       }).fail(function(status, error) {
-           console.log('oops something went wrong.', status);
-           console.log(error);
-       }).always(function() {
-           console.log('this always runs');
-       })
-   });
+(function () {
+  $(document).ready(function () {
+    // console.log("DOM is ready");
+    $(".generate").click(function () {
+      $.ajax("https://api.chucknorris.io/jokes/random")
+        .done(function (data, status) {
+        //   console.log("Everything is looking good.", status);
+          $(".joke").text(data.value);
+        })
+        .fail(function (status, error) {
+          console.log("oops something went wrong.", status);
+          console.log(error);
+        })
+        .always(function () {
+        //   console.log("this always runs");
+        });
+    });
+  });
 })();
